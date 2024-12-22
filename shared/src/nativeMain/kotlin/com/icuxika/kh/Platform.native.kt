30b112f77da17d24b7f600df760d107788a8fc6d
icuxika <icuxika@outlook.com>
2024-12-22 17:41:27 +0800

    Get native platform name

--- shared/src/nativeMain/kotlin/com/icuxika/kh/Platform.native.kt
+++ shared/src/nativeMain/kotlin/com/icuxika/kh/Platform.native.kt
@@ -1,7 +1,15 @@
 package com.icuxika.kh
 
+import kotlin.experimental.ExperimentalNativeApi
+
+@OptIn(ExperimentalNativeApi::class)
 class NativePlatform : Platform {
-    override val name: String = "Native"
+    override val name: String = when (kotlin.native.Platform.osFamily) {
+        OsFamily.WINDOWS -> OsFamily.WINDOWS.name
+        OsFamily.MACOSX -> OsFamily.MACOSX.name
+        OsFamily.LINUX -> OsFamily.LINUX.name
+        else -> "Native"
+    }
 }
 
 actual fun getPlatform(): Platform = NativePlatform()
